f13f8ebf68e2b7d11156ef530b0d40d544d4760c
kathy <mccdahl@gmail.com>
2020-07-14 16:53:00 +0100

    hi

--- __init__.py
+++ __init__.py
@@ -4,10 +4,12 @@ from mycroft import MycroftSkill, intent_file_handler
 class IDrankWater(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+	self.num = 0
 
     @intent_file_handler('water.drank.i.intent')
     def handle_water_drank_i(self, message):
-        self.speak_dialog('water.drank.i')
+        self.num += 1
+	self.speak_dialog('water.drank.i',{'num':str(self.num)})
 
 
 def create_skill():
